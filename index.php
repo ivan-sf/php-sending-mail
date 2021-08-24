75cf84fa0ff18a6b55be52feccee4ff2cbc8c331
MajorTom <ivansantander2020@gmail.com>
2021-08-24 10:49:47 -0500

    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <form action="mail.php" method="POST">
-    <input type="text" placeholder="Nombre" name="nombre" >
-    <input type="text" placeholder="Correo" name="correo" >
-    <input type="text" placeholder="Telefono" name="telefono" >
-    <input type="text" placeholder="Mensaje" name="mensaje" >
+    <input type="text" placeholder="Nombre" name="nombre" ><br>
+    <input type="text" placeholder="Correo" name="correo" ><br>
+    <input type="text" placeholder="Telefono" name="telefono" ><br>
+    <input type="text" placeholder="Mensaje" name="mensaje" ><br>
     <input type="submit" value="Enviar">
 </form>
